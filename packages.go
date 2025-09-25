496bd957e37c6d31082802ddb2e36c063b7ee7fe
noble-sun <matsubayashi.helio+github@gmail.com>
2025-09-25 18:35:55 -0300

    go tour - basics

---
+++ packages.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+	fmt.Println("My favorite number is", rand.Intn(10))
+}
+
+// A GO program is made up of packages, and every program start running in the
+// package main.
